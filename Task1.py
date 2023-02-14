44886f67c1346c2b6eba8ca5565a9283d04e9fa5
kirbytheone <leimtheone@gmail.com>
2023-02-14 16:50:45 +0200

    Initial commit1

---
+++ Task1.py
@@ -0,0 +1,4 @@
+import os
+import platform
+print('Current OS:', platform. uname(), 'Runs with Python version:', platform.python_version());
+# print('Current OS:', platform.system(), platform.version(), platform.release(), platform.architecture(), platform.machine(), 'Runs with Python version:', platform.python_version());
