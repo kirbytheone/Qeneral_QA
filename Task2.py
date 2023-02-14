72845a4b009c1bfad438b14b262d7361463443af
kirbytheone <leimtheone@gmail.com>
2023-02-14 23:33:54 +0200

    Initial commit2

--- Task2.py
+++ Task2.py
@@ -3,3 +3,14 @@ john_salary = 1000.50
 marta_salary = 921.2
 print('John salary:', john_salary, 'Marta salary', marta_salary);
 
+john_age = 33
+marta_age = 45
+print('John`s age:', john_salary, 'Marta`s age:', marta_age);
+
+john_name = "John Malkovitch"
+marta_name = "Marta Kaufman"
+print(john_name, marta_name);
+
+john_gender = False
+marta_gender = True
+print(john_gender, marta_gender);
